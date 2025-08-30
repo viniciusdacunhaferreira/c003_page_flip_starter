ef365ef9534005d7c8a41371391925b3db1472d4
Vinícius da Cunha Ferreira <viniciusdacunha.ferreira@gmail.com>
2025-08-30 19:35:25 -0300

    Initial Commit

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+
+## 0.1.1 - 2024-05-17
+
+- Use vector_graphics_compiler to precompile SVGs
+
+## 0.1.0
+
+- Added CHANGELOG
---
+++ lib/main.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'package:vector_graphics/vector_graphics.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          // Use Center as layout has unconstrained width (loose constraints),
+          // together with SizedBox to specify the max width (tight constraints)
+          // See this thread for more info:
+          // https://twitter.com/biz84/status/1445400059894542337
+          child: Center(
+            child: SizedBox(
+              width: 500, // max allowed width
+              child: HomePage(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Create PageFlipBuilder widget that can be used to flip between
+    // LightHomePage and DarkHomePage
+    return const LightHomePage();
+  }
+}
+
+class LightHomePage extends StatelessWidget {
+  const LightHomePage({super.key, this.onFlip});
+  final VoidCallback? onFlip;
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        brightness: Brightness.light,
+        textTheme: TextTheme(
+          displaySmall: Theme.of(context)
+              .textTheme
+              .displaySmall!
+              .copyWith(color: Colors.black87, fontWeight: FontWeight.w600),
+        ),
+      ),
+      child: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.all(24.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.red, width: 5),
+          ),
+          child: Column(
+            children: [
+              const ProfileHeader(prompt: 'Hello,\nsunshine!'),
+              const Spacer(),
+              const VectorGraphic(
+                loader: AssetBytesLoader('assets/forest-day.svg'),
+                semanticsLabel: 'Forest',
+                width: 300,
+                height: 300,
+              ),
+              const Spacer(),
+              BottomFlipIconButton(onFlip: onFlip),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DarkHomePage extends StatelessWidget {
+  const DarkHomePage({super.key, this.onFlip});
+  final VoidCallback? onFlip;
+
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+          brightness: Brightness.dark,
+          textTheme: TextTheme(
+            displaySmall: Theme.of(context)
+                .textTheme
+                .displaySmall!
+                .copyWith(color: Colors.white, fontWeight: FontWeight.w600),
+          )),
+      child: Scaffold(
+        body: Container(
+          padding: const EdgeInsets.all(24.0),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.red, width: 5),
+          ),
+          child: Column(
+            children: [
+              const ProfileHeader(prompt: 'Good night,\nsleep tight!'),
+              const Spacer(),
+              const VectorGraphic(
+                loader: AssetBytesLoader('assets/forest-night.svg'),
+                semanticsLabel: 'Forest',
+                width: 300,
+                height: 300,
+              ),
+              const Spacer(),
+              BottomFlipIconButton(onFlip: onFlip),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfileHeader extends StatelessWidget {
+  const ProfileHeader({super.key, required this.prompt});
+  final String prompt;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      bottom: false,
+      child: Row(
+        children: [
+          Text(prompt, style: Theme.of(context).textTheme.displaySmall),
+          const Spacer(),
+          const VectorGraphic(
+            loader: AssetBytesLoader('assets/man.svg'),
+            semanticsLabel: 'Profile',
+            width: 50,
+            height: 50,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BottomFlipIconButton extends StatelessWidget {
+  const BottomFlipIconButton({super.key, this.onFlip});
+  final VoidCallback? onFlip;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          onPressed: onFlip,
+          icon: const Icon(Icons.flip),
+        )
+      ],
+    );
+  }
+}
